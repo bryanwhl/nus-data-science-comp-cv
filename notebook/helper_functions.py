919a8dff6ea35e0843cdb5e51aa0b6c03e81b875
Uxinnn <xinyu.foong@gmail.com>
2021-01-19 19:28:48 +0800

    match by img id

--- notebook/helper_functions.py
+++ notebook/helper_functions.py
@@ -4,4 +4,4 @@ import os
 
 # Takes the image directory path and output a list of image matrices
 def read_imgs(path):
-    return [cv.imread(os.path.join(path, img)) for img in os.listdir(path)]
+    return {int(img.split(".")[0]): cv.imread(os.path.join(path, img)) for img in os.listdir(path)}
